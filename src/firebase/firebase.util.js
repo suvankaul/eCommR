de24ab0b312edd80dd90b006200e15cf8ab7ea0b
Suvan Kaul <suvankaul97@gmail.com>
2020-08-18 23:43:19 +0530

    config changes

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -1,16 +1,9 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAf-3aAANJEcPs_4XxzDp77OOn60ytOn7k",
-    authDomain: "sk-clothing.firebaseapp.com",
-    databaseURL: "https://sk-clothing.firebaseio.com",
-    projectId: "sk-clothing",
-    storageBucket: "sk-clothing.appspot.com",
-    messagingSenderId: "607764541298",
-    appId: "1:607764541298:web:4f9d6b35ddb2d25a39f7eb"
-};
+const firebaseConfig = firebaseConfig;
 
 //--------------------------------------Initialise firebase for project----------------------------------------------------------------
 firebase.initializeApp(firebaseConfig);
