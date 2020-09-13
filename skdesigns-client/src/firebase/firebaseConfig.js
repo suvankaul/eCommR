fb2b8bc3b01ce725a7ecc8a7002ac6de945e335a
Suvan Kaul <suvankaul97@gmail.com>
2020-09-13 21:41:41 +0530

    changes in heroku config

---
+++ skdesigns-client/src/firebase/firebaseConfig.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyAf-3aAANJEcPs_4XxzDp77OOn60ytOn7k",
+    authDomain: "sk-clothing.firebaseapp.com",
+    databaseURL: "https://sk-clothing.firebaseio.com",
+    projectId: "sk-clothing",
+    storageBucket: "sk-clothing.appspot.com",
+    messagingSenderId: "607764541298",
+    appId: "1:607764541298:web:4f9d6b35ddb2d25a39f7eb"
+};
+
+export default config;
